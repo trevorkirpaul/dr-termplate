1fa5f11c98bc36fa7d860d59b87a32824bbe375a
Trevor Kirpaul <trevorkirpaul@gmail.com>
2023-08-26 15:27:10 -0400

    - update Location view

--- src/pages/Location/index.tsx
+++ src/pages/Location/index.tsx
@@ -1,12 +1,22 @@
 import React from "react";
-import Header from "../../components/Header";
+import PageContainer from "../../components/PageContainer";
+import GoogleMapsFrame from "../../components/GoogleMapsFrame";
 
 const Location: React.FC = () => {
   return (
-    <>
-      <Header />
-      <h1 className="text-green-400">Location</h1>
-    </>
+    <PageContainer>
+      <h1 className="mb-5 flex text-xl">Location</h1>
+      <div className="mb-5">
+        <p>8701 Old Troy Pike</p>
+        <p className="mb-2">Huber Heights, OH 45424</p>
+        <a className="mb-2 inline-block underline" href="tel:123-456-7890">
+          (123) 456-7890
+        </a>
+        <p>Hours:</p>
+        <p>8am - 5pm</p>
+      </div>
+      <GoogleMapsFrame />
+    </PageContainer>
   );
 };
 
