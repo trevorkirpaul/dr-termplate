a0b7abd38b231c343668d42926f152042da472d2
Trevor Kirpaul <trevorkirpaul@gmail.com>
2023-08-26 11:29:59 -0400

    - install react-hook-form - create form component - udpate Contact View

---
+++ src/components/Form/index.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { Cancel } from "iconoir-react";
+
+interface Field {
+  label: string;
+  name: string;
+  required?: string;
+  inputType?: "input" | "textArea";
+  type?: "string" | "tel";
+}
+
+export interface FormOptions {
+  fields: Field[];
+}
+
+interface IProps {
+  onSubmit: SubmitHandler<any>;
+  formOptions: FormOptions;
+}
+
+const inputClassName =
+  "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5";
+
+const getInputClass = (x?: Object) =>
+  x ? inputClassName + " border-red-300" : inputClassName;
+
+const Form: React.FC<IProps> = ({ onSubmit, formOptions }) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<any>();
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      {formOptions.fields.map((fo) => (
+        <div className="mb-5">
+          <label className="block">{fo.label}</label>
+          {fo.inputType !== "textArea" && (
+            <input
+              className={getInputClass(errors?.[fo.name])}
+              type={fo.type || "text"}
+              {...register(fo.name, { required: fo.required })}
+            />
+          )}
+
+          {fo.inputType === "textArea" && (
+            <textarea
+              className={getInputClass(errors?.[fo.name])}
+              {...register(fo.name, { required: fo.required })}
+            />
+          )}
+          {errors?.[fo.name] && (
+            <span className="text-red-500 block my-2 flex">
+              <Cancel className="inline" /> {errors[fo.name]?.message}
+            </span>
+          )}
+        </div>
+      ))}
+
+      <input
+        type="submit"
+        className="bg-blue-400 px-3 py-2 rounded cursor-pointer"
+      />
+    </form>
+  );
+};
+
+export default Form;
--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -1,10 +1,54 @@
 import React from "react";
+import { SubmitHandler } from "react-hook-form";
+import { Mail } from "iconoir-react";
+
+import Form, { FormOptions } from "../../components/Form";
 import PageContainer from "../../components/PageContainer";
 
+type Inputs = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+};
+
+const formOptions: FormOptions = {
+  fields: [
+    {
+      name: "firstName",
+      label: "First Name",
+      required: "First name is required",
+    },
+    { name: "lastName", label: "Last Name", required: "Last name is required" },
+    { name: "email", label: "Email", required: "Email is required" },
+    {
+      name: "phone",
+      label: "Phone",
+      type: "tel",
+      required: "Phone is required",
+    },
+    {
+      name: "message",
+      label: "Message",
+      required: "Message is required",
+      inputType: "textArea",
+    },
+  ],
+};
+
 const Contact: React.FC = () => {
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
   return (
     <PageContainer>
-      <h1 className="text-green-400">Contact</h1>
+      <h1 className="text-xl mb-5 flex">
+        <Mail className="inline mr-2" /> Contact
+      </h1>
+      <p className="mb-5">
+        Got questions or need to schedule an appointment? We're here to help.
+        Fill out the form below, and we'll get back to you as soon as possible.
+      </p>
+      <Form formOptions={formOptions} onSubmit={onSubmit} />
     </PageContainer>
   );
 };
